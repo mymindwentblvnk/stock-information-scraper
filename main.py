e76e4e81c62bfe19cf70e8b4ace2c729c5c85340
Michael Eichenseer <16592938+mymindwentblvnk@users.noreply.github.com>
2023-05-26 10:59:31 +0200

    Add more resilient scraper.

---
+++ main.py
@@ -0,0 +1,65 @@
+import csv
+from typing import List
+
+from data_providers import StockInformation, get_years, StockInformationResult
+
+
+def save_numbers_to_csv(information: List[StockInformationResult], file_name: str):
+    min_year = min([i.min_year for i in information])
+    max_year = max([i.max_year for i in information])
+    years = list(reversed(range(min_year, max_year + 1)))
+
+    # Build header
+    header = []
+    header.append('ticker')
+    header.extend([f'Return On Capital ({year})' for year in years])
+    header.extend([f'Book Value per Share ({year})' for year in years])
+    header.extend([f'EPS (Diluted) ({year})' for year in years])
+    header.extend([f'Revenue ({year})' for year in years])
+    header.extend([f'Free Cash Flow Per Share ({year})' for year in years])
+    header.append('Growth Estimates - Next 5 Years')
+    header.append('PE Ratio (Min)')
+    header.append('PE Ratio (Max)')
+
+    data = []
+    # Add numbers
+    for ticker_info in information:
+        # Build array with numbers
+        ticker_numbers = [ticker_info.ticker]
+        # Return Of Capital (ROIC)
+        roic = ticker_info.return_on_capital_roic
+        ticker_numbers.extend([roic[year] for year in years])
+        # Book Value per Share
+        bvps = ticker_info.book_value_per_share
+        ticker_numbers.extend([bvps[year] if bvps[year] else '' for year in years])
+        # EPS (Diluted)
+        eps = ticker_info.book_value_per_share
+        ticker_numbers.extend([eps[year] if eps[year] else '' for year in years])
+        # Revenue
+        revenue = ticker_info.book_value_per_share
+        ticker_numbers.extend([revenue[year] if revenue[year] else '' for year in years])
+        # Free Cash Flow Per Share
+        fcfps = ticker_info.free_cash_flow_per_share
+        ticker_numbers.extend([fcfps[year] if fcfps[year] else '' for year in years])
+        # Growth Estimates - Next 5 Years
+        ticker_numbers.append(ticker_info.growth_estimates_next_5_years)
+        # PE Ratio
+        ticker_numbers.append(ticker_info.pe_ratio_min)
+        ticker_numbers.append(ticker_info.pe_ratio_max)
+        assert len(header) == len(ticker_numbers)
+
+        # Add to data
+        data.append(ticker_numbers)
+
+    csv_data = [header]
+    csv_data.extend(data)
+
+    with open(file_name, 'w') as ticker_numbers_csv:
+        csv.writer(ticker_numbers_csv, delimiter=",").writerows(csv_data)
+
+
+if __name__ == '__main__':
+    all_tickers = ['META', 'AAPL', 'MSFT', 'GOOGL', 'AMD', 'AMZN', 'LLY', 'NVDA', 'FSLR', 'PERI', 'TSLA', 'AGYS', 'NVO', 'WIRE', 'JBL', 'ENPH', 'SPOT', 'ASML', 'PANW', 'CI']
+    years = get_years()
+    all_numbers = [StockInformation(t).get_all_values(years) for t in all_tickers]
+    save_numbers_to_csv(all_numbers, 'numbers.csv')
--- test.py
+++ test.py
@@ -1,26 +1,49 @@
-from hamcrest import assert_that, equal_to, has_length
 import unittest
 
-from scraper import get_numbers_for_ticker, CSV_HEADER
+from hamcrest import assert_that, equal_to, has_length
+
+from data_providers import get_years, StockInformation, DataType
 
 
 class TestScraper(unittest.TestCase):
 
     def test_META_stock_information(self):
-        meta_numbers = get_numbers_for_ticker("META")
-        assert_that(meta_numbers, has_length(55))
-        assert_that(meta_numbers[CSV_HEADER.index('ticker')], equal_to('META'))
-        assert_that(meta_numbers[CSV_HEADER.index('roic_2019')], equal_to(30.10))
-        assert_that(meta_numbers[CSV_HEADER.index('bvps_2013')], equal_to(6.39))
-        assert_that(meta_numbers[CSV_HEADER.index('eps_diluted_2022')], equal_to(8.59))
-        assert_that(meta_numbers[CSV_HEADER.index('revenue_2020')], equal_to(85965))
-        assert_that(meta_numbers[CSV_HEADER.index('fcfps_2014')], equal_to(2.10))
-        assert_that(meta_numbers[CSV_HEADER.index('growth_estimates')], equal_to(10.6))
-        assert_that(meta_numbers[CSV_HEADER.index('pe_ratio_min')], equal_to(8.476))
-        assert_that(meta_numbers[CSV_HEADER.index('pe_ratio_max')], equal_to(37.11))
+        years = get_years(number_of_years=11)
+        info = StockInformation('META').get_all_values(years)
+        assert_that(info.ticker, equal_to('META'))
+        assert_that(info.return_on_capital_roic[2019], equal_to(30.10))
+        assert_that(info.book_value_per_share[2013], equal_to(6.39))
+        assert_that(info.eps_diluted[2022], equal_to(8.59))
+        assert_that(info.revenue[2020], equal_to(85965))
+        assert_that(info.free_cash_flow_per_share[2014], equal_to(2.10))
+        # This changes regularly. See https://www.zacks.com/stock/quote/META/detailed-earning-estimates for current value
+        # assert_that(info.growth_estimates_next_5_years, equal_to(19.5))
+        assert_that(info.pe_ratio_min, equal_to(8.476))
+        assert_that(info.pe_ratio_max, equal_to(37.11))
 
     def test_LLY_stock_information(self):
-        lly_numbers = get_numbers_for_ticker('LLY')
-        assert_that(lly_numbers[CSV_HEADER.index('revenue_2022')], equal_to(28541.4))
+        revenue_2022 = StockInformation('LLY').get_value(DataType.REVENUE, 2022)
+        assert_that(revenue_2022, equal_to(28541.4))
+
+    def test_PANW_stock_information(self):
+        pe_ratio_min = StockInformation('PANW').get_value(DataType.PE_RATIO_MIN, -1)
+        assert_that(pe_ratio_min, equal_to(280.78))
+
+    def test_max_min_year(self):
+        info = StockInformation('GOGL').get_all_values([2023, 2022, 2019, 2018, 2000])
+        assert_that(info.max_year, equal_to(2022))  # Since 2023 is not closed yet
+        assert_that(info.min_year, equal_to(2018))  # Since we can only retrieve data for 10 years, 2000 will be None
 
 
+class TestGetYears(unittest.TestCase):
+
+    def test_get_years_desc(self):
+        years = get_years(start_year=2000, number_of_years=3, desc=True)
+        assert_that(years, equal_to([2000, 1999, 1998]))
+
+    def test_get_years_asc(self):
+        years = get_years(start_year=2000, number_of_years=3, desc=False)
+        assert_that(years, equal_to([2000, 2001, 2002]))
+
+    def test_get_years_returns_10_years(self):
+        assert_that(get_years(), has_length(10))
